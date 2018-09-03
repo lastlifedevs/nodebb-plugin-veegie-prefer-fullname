e0772836b48f80ec2881d4ea32a5d84722f0760b
Veegie <jbalanuik@gmail.com>
2018-09-02 20:45:30 -0400

    Try alternate iteration method, log object for debugging.

--- library.js
+++ library.js
@@ -1,12 +1,20 @@
 "use strict";
 
-var async = require('async');
-
 var user = module.parent.require('./user');
 var controllers = require('./lib/controllers');
 var preferFullname = {};
 
 preferFullname.init = function(params, callback) {
+	var router = params.router,
+		hostMiddleware = params.middleware,
+		hostControllers = params.controllers;
+		
+	// We create two routes for every view. One API call, and the actual route itself.
+	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
+
+	router.get('/admin/plugins/preferfullname', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get('/api/admin/plugins/preferfullname', controllers.renderAdminPage);
+
 	callback();
 };
 
@@ -20,13 +28,15 @@ preferFullname.addAdminNavigation = function(header, callback) {
 	callback(null, header);
 };
 
+
 preferFullname.getFullname = function (data, callback) {
 	if (data.uid === 0) {
 		callback(null, data);
 	} else {
-		async.each(data, function(teaser, callback) {
+		data.teasers.foreach(function(teaser) {
+			console.log(teaser);
 			teaser.user.fullname = user.getUserField(teaser.user.uid, 'fullname', callback);
-		}, callback);
+		});
 	}
 }
 
