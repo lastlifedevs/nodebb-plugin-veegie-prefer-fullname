a7f9c8372c053479e85f08ab5350fd24e37e0c55
Veegie <jbalanuik@gmail.com>
2018-09-05 19:41:44 -0400

    Remove unnecessary boilerplate contents.

--- library.js
+++ library.js
@@ -1,34 +1,8 @@
 "use strict";
 
 var async = require('async');
-
 var user = module.parent.require('./user');
-var controllers = require('./lib/controllers');
 var preferFullname = {};
-
-preferFullname.init = function(params, callback) {
-	var router = params.router,
-		hostMiddleware = params.middleware,
-		hostControllers = params.controllers;
-		
-	// We create two routes for every view. One API call, and the actual route itself.
-	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
-
-	router.get('/admin/plugins/preferfullname', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/preferfullname', controllers.renderAdminPage);
-
-	callback();
-};
-
-preferFullname.addAdminNavigation = function(header, callback) {
-	header.plugins.push({
-		route: '/plugins/preferfullname',
-		icon: 'fa-tint',
-		name: 'Prefer Full Name'
-	});
-
-	callback(null, header);
-};
 
 preferFullname.getFullname = function (data, callback) {
 	if (data.uid === 0) {
